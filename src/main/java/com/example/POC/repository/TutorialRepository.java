640717e7ca459f6856be95ac226de4ffb50f1931
Subham Das <subhamd@emids.com>
2022-07-05 09:58:46 +0530

    [subham] : Added jdbc template

---
+++ src/main/java/com/example/POC/repository/TutorialRepository.java
@@ -0,0 +1,16 @@
+package com.example.POC.repository;
+
+import com.example.POC.model.Tutorial;
+
+import java.util.List;
+
+public interface TutorialRepository {
+    int save(Tutorial book);
+    int update(Tutorial book);
+    Tutorial findById(Long id);
+    int deleteById(Long id);
+    List<Tutorial> findAll();
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+    int deleteAll();
+}
